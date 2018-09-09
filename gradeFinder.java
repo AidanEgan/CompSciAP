857557ae2158df8f206d6f80b7988b3c66d71c4c
AidanEgan <aidanegan19@gmail.com>
2018-09-09 15:13:19 -0400

    Made it more foolproof

--- gradeFinder.java
+++ gradeFinder.java
@@ -7,11 +7,17 @@ public class gradeFinder {
 		System.out.println("What is your number grade? ");
 		double grade = input.nextDouble();
 		input.close();
-		if (Math.floor(grade/10) < 5){
-			grade = 50;
+		if (grade > 99){
+			grade = 99;
 		}
 		int newGrade = (int)((grade/10)-5);
 		int newAppend = (int)(Math.ceil((grade%10)/3)-1);
+		if (Math.floor(grade/10) < 6){
+			newGrade = 0;
+			newAppend = 1;
+		} else if(newGrade == 4 && newAppend == 2){
+			newAppend = 1;
+		}
 		System.out.println("Your letter grade is " + gradeArray[newGrade] + gradeAppend[newAppend]);
 	}
 }
