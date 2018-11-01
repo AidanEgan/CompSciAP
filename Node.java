a945b6b87f8b22fe89e8af381d81106c0cc761b4
AidanEgan <aidanegan19@gmail.com>
2018-11-01 14:32:00 -0400

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,24 @@
+public class Node {
+	private int data;
+	private Node next;
+	public Node() {
+		data = 1;
+		next = null;
+	}
+	public Node(int i, Node n) {
+		data = i;
+		next = n;
+	}
+	public void setData(int m) {
+		data = m;
+	}
+	public int getData() {
+		return data;
+	}
+	public void setNext(Node n) {
+		next = n;
+	}
+	public Node getNext() {
+		return next;
+	}
+}
