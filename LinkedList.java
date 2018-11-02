cf36d87ba828a70bf2775e06b8655f11026a2ca5
AidanEgan <aidanegan19@gmail.com>
2018-11-01 20:13:02 -0400

    Update LinkedList.java
    Added some more methods and stuff

--- LinkedList.java
+++ LinkedList.java
@@ -11,21 +11,22 @@ public class LinkedList {
 		head = null;
 	}
 	public void print() {
-		
-	}
-	public Node getToEnd(Node n) {
-		n = head;
-		while(n.getNext() != null) {
+		Node n = head;
+		while(n != null){
+			System.out.print("<" + n.getData() + "> ");
 			n = n.getNext();
 		}
-		return n;
 	}
 	public void insertAtFront(Node N) {
 		N.setNext(head);
 		head = N;
 	}
 	public void insertAtEnd(Node N) {
-		getToEnd(head).setNext(N);
+		Node w = head;
+		while(w.getNext() != null) {
+			w = w.getNext();
+		}
+		w.setNext(N);
 	}
 	public void removeHead() {
 		head = head.getNext();
@@ -38,13 +39,34 @@ public class LinkedList {
 		n.setNext(null);
 	}
 	public void removeNode (Node p) {
-		
+		Node prev = head;
+		while(prev.getNext() != p){
+			prev = prev.getNext();
+		}
+		prev.setNext(p.getNext());
+	}
+	
+	public int insertNode(Node p, int i){
+		Node n = head;
+		int current = 0;
+		while(current < (i-1)){
+			if(n == null){
+				System.out.println("Index does not exist");
+				return 0;
+			}else{
+				n = n.getNext();
+			}
+			current++;
+		}
+		p.setNext(n.getNext());
+		n.setNext(p);
+		return 1;
 	}
 	
 	public int countNodes() {
-		int numOfNodes = 1;
+		int numOfNodes = 0;
 		Node temp = head;
-		while(temp.getNext() != null) {
+		while(temp != null) {
 			numOfNodes++;
 			temp = temp.getNext();
 		}
@@ -55,14 +77,30 @@ public class LinkedList {
 	}
 	
 	public static void  main(String[] args) {
+		//Define the Nodes I'll be using for tests
 		Node n2 = new Node();
-		Node newFront = new Node();
 		Node n1 = new Node(10,n2);
 		Node n = new Node(25,n1);
+		Node newFront = new Node(32);
+		Node newEnd = new Node(19);
+		Node insertNode = new Node(57);
 		
+		//Create a test LinkedList -> <25,10,1>
 		LinkedList l = new LinkedList(n);
-		System.out.println(l.countNodes());
+		//Test the class' methods
+		//Insert a Node of value 35 at the front
 		l.insertAtFront(newFront);
+		//Should return a 4
 		System.out.println(l.countNodes());
+		//Insert a value of 19 at the end
+		l.insertAtEnd(newEnd);
+		//Print out the LinkedList so far
+		l.print();
+		System.out.println();
+		//l.removeEnd();
+		//Try the insert
+		l.insertNode(insertNode, 8);
+		//Print it out again with one less at the end
+		l.print();
 	}
 }
